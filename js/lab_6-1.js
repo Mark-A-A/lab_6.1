6fa9a90dc675ac783ab198c119183f578bc08e72
Mark-A-A <markawai01@gmail.com>
2015-12-02 11:34:45 -0500

    added twitter pic jQuery function, 403 error with picture

---
+++ js/lab_6-1.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+  alert("Hello World");
+
+
+  for (var i = 0; i < window.tweets.length; i++) {
+    var tweetCol = tweetBuild (window.tweets[i]);
+    console.log(tweetCol);
+    $(".row").append(tweetCol)
+  };
+
+  function tweetBuild(tweeter) {
+    var colDiv = $("<div>").addClass("col-md-4");
+    var pictureDiv = $("<div>").addClass("picture");
+    var tweetPic = $("<img>").attr("src",  tweeter.user.profile_image_url)
+    var twitterName;
+    var tweetedWhen;
+    var tweet;
+
+    console.log(tweetPic);
+    console.log(colDiv);
+    colDiv.append(pictureDiv);
+    pictureDiv.append(tweetPic)
+  }; //end of function
+
+});
